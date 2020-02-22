6e18f171422c35583473c0538614758c7acb8766
MuhammadWasif <wasif33@outlook.com>
2020-02-22 20:47:28 +0500

    bug fixes and improvements

--- index.js
+++ index.js
@@ -4,23 +4,27 @@ const chalk = require("chalk");
 const http = require("http");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+const EventEmitter = require("events");
+
+class Job extends EventEmitter {}
+const job = new Job();
+
+job.on("received", () => {
+  console.log("Response received");
+  makeGetRequest(url);
+});
 
 // fucntion that make a GET request on every URL to check if it's not broken
 async function makeGetRequest(link) {
-  console.log(link);
-  if (link == "/") {
-    chalk.dim("Invalid URL");
-  } else {
-    await request(link, (er, data) => {
-      if (er) console.log(er);
-      let statusCode = data.statusCode;
-      if (statusCode != 404) {
-        console.log(chalk.green(`SUCCESS`), link);
-      } else {
-        console.log(chalk.red(`BROKEN`), link);
-      }
-    });
-  }
+  request(link, (er, data) => {
+    if (er) console.log(er);
+    let statusCode = data.statusCode;
+    if (statusCode != 404) {
+      console.log(chalk.green(`SUCCESS`), link);
+    } else {
+      console.log(chalk.red(`BROKEN`), link);
+    }
+  });
 }
 
 console.log("Process is running...");
@@ -29,7 +33,7 @@ First, download the HTML of given web page by making a GET request.
 Create an HTML file and host it locally. Make a request on localhost.
 This step is necessary to get all links from DOM
 */
-request("https://wasif.codes", (req, res) => {
+request("https://noman-gul.firebaseapp.com/", (req, res) => {
   const body = res.body;
 
   fs.writeFile("index.html", body, (er, data) => {
@@ -52,13 +56,28 @@ request("https://wasif.codes", (req, res) => {
         for (let link of dom.window.document.links) links.push(link.href);
         let uniqueLinks = [...new Set(links)];
         console.log(`${uniqueLinks.length} unique URLs found on website`);
-        for (let link of uniqueLinks) {
-          makeGetRequest(link);
-          if (link == uniqueLinks[uniqueLinks.length - 1]) {
-            chalk.bold("This was last URL");
+
+        function loop(n) {
+          if (n == uniqueLinks.length) {
+            chalk.greenBright("All links have been checked!");
             process.exit();
+          } else {
+            request(uniqueLinks[n], (er, data) => {
+              if (er) return er;
+              let statusCode = data.statusCode;
+              if (statusCode != 404) {
+                console.log(chalk.green(`SUCCESS`), uniqueLinks[n]);
+                n++;
+                loop(n);
+              } else {
+                console.log(chalk.red(`BROKEN`), uniqueLinks[n]);
+                n++;
+                loop(n);
+              }
+            });
           }
         }
+        loop(1);
       });
     });
   });
